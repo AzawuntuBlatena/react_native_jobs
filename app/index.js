fb4fd25a991b9e748518d8e88deda33f69137279
AzawuntuBlatena <azabla1227@gmail.com>
2024-06-30 07:11:31 +0300

    done!

--- app/index.js
+++ app/index.js
@@ -1,38 +1,5 @@
-import {useState} from 'react'
-import { View, Text, ScrollView, SafeAreaView } from 'react-native'
-import { Stack, useRouter } from 'expo-router'
-import {icons, images, COLORs, SIZES, COLORS} from "../constants"
-import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components'
+import { Redirect } from "expo-router";
 
-const Home = () => {
-    const router = useRouter()
-  const [search, setSearch] = useState(false)
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
-      <Stack.Screen 
-      options={{
-        headerStyle: {backgroundColor: COLORS.lightWhite},
-        headerShadowVisible: false,
-        headerLeft: () => (
-            <ScreenHeaderBtn iconUrl= {icons.menu} dimension = "60%"/>
-        ),
-        headerRight: ()=> (
-            <ScreenHeaderBtn iconUrl= {images.profile} dimension = "100%"/>
-        ),
-        headerTitle: ""
-      }}/>
-      <ScrollView showsVerticalScrollIndicator = {false}>
-        <View style= {{
-            flex: 1,
-            padding: SIZES.medium
-        }}>
-            <Welcome/>
-            <Popularjobs/>
-            <Nearbyjobs/>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  )
+export default function Index() {
+    return <Redirect href="/home" />;
 }
-
-export default Home
